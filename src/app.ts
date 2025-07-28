b9c02cfb301cda30196a6bdbb7fe87ed11617cdf
nemnem202 <naimelhabbas@gmail.com>
2025-07-28 17:05:26 +0200

    init

---
+++ src/app.ts
@@ -0,0 +1,12 @@
+import express from "express";
+
+const app = express();
+const PORT = 3000;
+
+app.get("", (req, res) => {
+  res.send("coucou !");
+});
+
+app.listen(PORT, () => {
+  console.log(`Serveur démarré sur http://localhost:${PORT}`);
+});
